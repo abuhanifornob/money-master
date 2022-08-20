8623b20c13e017e78eb8934f6c571c55bd652cbb
Abu Hanif Arnob <hanifcse90@gmail.com>
2022-08-20 20:56:12 +0600

    coding

--- js/money.js
+++ js/money.js
@@ -1,2 +1,20 @@
-const incomeField = document.getElementById('inputIcome');
-const income = (parseFloat(incomeField.value)).toFixed(2);
+function expencesCalculate() {
+    const incomeField = document.getElementById('inputIcome');
+    const income = (parseFloat(incomeField.value)).toFixed(2);
+    const foodField = document.getElementById('food');
+    const food = (parseFloat(foodField.value)).toFixed(2);
+    const rentField = document.getElementById('rent');
+    const rent = (parseFloat(rentField.value)).toFixed(2);
+    const clothesFiled = document.getElementById('clothes');
+    const clothes = (parseFloat(clothesFiled.value)).toFixed(2);
+    const totalExpences = food.value + rent.value + clothes.value;
+    const expencesElement = document.getElementById('totalExpences');
+    expencesElement.innerText = totalExpences;
+    const totalBalance = income - totalExpences;
+    const balanceElement = document.getElementById('totalBalance');
+    balanceElement.innerText = totalBalance;
+
+}
+document.getElementById('calculatorBtn').addEventListener('click', function() {
+    expencesCalculate();
+})
